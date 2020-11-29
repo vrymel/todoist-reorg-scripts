91b7e6b0a866b05a5e5769dc8e2871a612a1247a
Vrymel <vrymel.dev@gmail.com>
2020-11-29 16:52:59 +0800

    Promote P2 to P1

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import os
 import todoist
 
-api = todoist.TodoistAPI('68df7a8351bb221682350ce1f745eb2c86471ac7')
+api = todoist.TodoistAPI(os.environ.get('TOKEN'))
 api.reset_state()
 todoist_data = api.sync()
 
+if 'error_tag' in todoist_data:
+    print('Could not get data from Todoist: ' + todoist_data['error_tag'])
+    exit(1)
+
 # P1 is actually priority 4 in the API
+# P2 is actually priority 3 in the API
 PRIORITY_MAP = {
     '1': 4,
     '2': 3
@@ -21,7 +27,7 @@ def get_project(project_name):
     return None
 
 
-def get_project_tasks(project):
+def get_project_items(project):
     project_tasks = []
 
     for item in todoist_data['items']:
@@ -47,9 +53,23 @@ def purge_other_p1_items(items):
         api.commit()
 
 
+def promote_p2_to_p1_item(items):
+    p2_tasks = []
+
+    for item in items:
+        if item['priority'] == PRIORITY_MAP['2']:
+            p2_tasks.append(item)
+
+    latest_p2_item = p2_tasks.pop()
+
+    if latest_p2_item:
+        item_model = api.items.get_by_id(latest_p2_item['id'])
+        item_model.update(priority=PRIORITY_MAP['1'])
+        ret = api.commit()
+
+
 # todo: pass project name as argument from Alfred
 target_project = get_project('Concentrix')
-tasks = get_project_tasks(target_project)
-purge_other_p1_items(tasks)
-if tasks:
-    pass
+tasks = get_project_items(target_project)
+# purge_other_p1_items(tasks)
+# promote_p2_to_p1_item(tasks)
