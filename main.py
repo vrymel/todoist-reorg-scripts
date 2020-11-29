152c16863cfd203e14c5ac640b3eca7ea6eb4455
Vrymel <vrymel.dev@gmail.com>
2020-11-29 16:37:09 +0800

    Purge other P1 tasks

--- main.py
+++ main.py
@@ -4,15 +4,11 @@ api = todoist.TodoistAPI('68df7a8351bb221682350ce1f745eb2c86471ac7')
 api.reset_state()
 todoist_data = api.sync()
 
-
-def get_project_tasks(project):
-    project_tasks = []
-
-    for item in todoist_data['items']:
-        if item['project_id'] == project['id']:
-            project_tasks.append(item)
-
-    return project_tasks
+# P1 is actually priority 4 in the API
+PRIORITY_MAP = {
+    '1': 4,
+    '2': 3
+}
 
 
 def get_project(project_name):
@@ -25,8 +21,35 @@ def get_project(project_name):
     return None
 
 
+def get_project_tasks(project):
+    project_tasks = []
+
+    for item in todoist_data['items']:
+        if item['project_id'] == project['id']:
+            project_tasks.append(item)
+
+    return project_tasks
+
+
+def purge_other_p1_items(items):
+    p1_tasks = []
+
+    for item in items:
+        if item['priority'] == PRIORITY_MAP['1']:
+            p1_tasks.append(item)
+
+    # pop latest so we wont include it in our update
+    p1_tasks.pop()
+
+    for item in p1_tasks:
+        item_model = api.items.get_by_id(item['id'])
+        item_model.update(priority=PRIORITY_MAP['2'])
+        api.commit()
+
+
 # todo: pass project name as argument from Alfred
-project = get_project('PSE Lookup')
-tasks = get_project_tasks(project)
+target_project = get_project('Concentrix')
+tasks = get_project_tasks(target_project)
+purge_other_p1_items(tasks)
 if tasks:
     pass
